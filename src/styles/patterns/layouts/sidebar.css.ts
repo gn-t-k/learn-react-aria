6882cbad609fa5acbf8ab521bbade9a2e520ad46
gn-t-k <gntk1995422@gmail.com>
2024-12-21 16:44:47 +0900

    sidebar

---
+++ src/styles/patterns/layouts/sidebar.css.ts
@@ -0,0 +1,19 @@
+// .with-sidebar {
+//   display: flex;
+//   flex-wrap: wrap;
+//   gap: var(--gutter, var(--s1))
+// }
+
+// .with-sidebar > :first-child {
+//   /* サイドバーがサイドバーたりうる値 */
+//   flex-basis: 20rem;
+//   flex-grow: 1
+// }
+
+// .with-sidebar > :last-child {
+//   /* 0から伸長する */
+//   flex-basis: 0;
+//   flex-grow: 999;
+//   /* 要素の幅が等しくなった場合に折り返す */
+//   min-width: 50%;
+// }
