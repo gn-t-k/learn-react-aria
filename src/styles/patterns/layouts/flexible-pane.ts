f7001cc816c330a817c50323b69dc6af52d0444b
gn-t-k <gntk1995422@gmail.com>
2024-12-25 17:17:13 +0900

    flexible-pane

---
+++ src/styles/patterns/layouts/flexible-pane.ts
@@ -0,0 +1,29 @@
+import { styleVariants } from "@vanilla-extract/css";
+import { ValueOf } from "../../../utils/value-of";
+import { tokens } from "../../theme.css";
+
+type Props =
+  | {
+      fixedWidth?: ValueOf<typeof tokens.width> | undefined;
+      flexibleMinWidth?: ValueOf<typeof tokens.width> | undefined;
+      gutter?: ValueOf<typeof tokens.spacing> | undefined;
+    }
+  | undefined;
+export const flexiblePane = (props?: Props) => {
+  return styleVariants({
+    wrapper: {
+      display: "flex",
+      flexWrap: "wrap",
+      gap: props?.gutter ?? tokens.spacing["2"],
+    },
+    fixed: {
+      flexBasis: props?.fixedWidth ?? tokens.width["20"],
+      flexGrow: 1,
+    },
+    flexible: {
+      flexBasis: 0,
+      flexGrow: 999,
+      minWidth: props?.flexibleMinWidth ?? "50%",
+    },
+  });
+};
